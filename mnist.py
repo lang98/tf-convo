1dabd546a42ae87364088357fab83749ed240adf
Ryan Qin <qinang5543@gmail.com>
2019-05-31 12:49:46 -0400

    add comments

--- mnist.py
+++ mnist.py
@@ -44,23 +44,23 @@ def normal_full_layer(input_layer, size):
 
 
 # placeholder
-x = tf.placeholder(tf.float32, shape=(None, 784))
-y_true = tf.placeholder(tf.float32, shape=[None, 10])
+x = tf.placeholder(tf.float32, shape=(None, 784))  # (batch, pixel)
+y_true = tf.placeholder(tf.float32, shape=[None, 10])  # onehot
 
 # layers
 x_image = tf.reshape(x, [-1, 28, 28, 1])
-convo_1 = convolutional_layer(x_image, shape=[5, 5, 1, 32])
+convo_1 = convolutional_layer(x_image, shape=[5, 5, 1, 32])  # 32 features for 5by5 patch, greyscale 1
 convo_1_pooling = max_pool_2b2(convo_1)
-convo_2 = convolutional_layer(convo_1_pooling, shape=[5, 5, 32, 64])
+convo_2 = convolutional_layer(convo_1_pooling, shape=[5, 5, 32, 64])  # 64 features for 5by5 patch
 convo_2_pooling = max_pool_2b2(convo_2)
 
 convo_2_flat = tf.reshape(convo_2_pooling, [-1, 7 * 7 * 64])
-full_layer_one = tf.nn.relu(normal_full_layer(convo_2_flat, 1024))
+full_layer_one = tf.nn.relu(normal_full_layer(convo_2_flat, 1024))  # 1024 neurons
 
 # dropout
 hold_prob = tf.placeholder(tf.float32)
 full_one_dropout = tf.nn.dropout(full_layer_one, keep_prob=hold_prob)
-y_pred = normal_full_layer(full_one_dropout, 10)
+y_pred = normal_full_layer(full_one_dropout, 10)  # 10 labels
 
 # loss function
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y_pred))
@@ -73,13 +73,13 @@ with tf.Session() as sess:
     sess.run(init)
     for i in range(steps):
         batch_x, batch_y = mnist.train.next_batch(50)
-        sess.run(train, feed_dict={x: batch_x, y_true: batch_y, hold_prob: 0.5})
+        sess.run(train, feed_dict={x: batch_x, y_true: batch_y, hold_prob: 0.5})  # drop half neurons
 
-        if i % 100 == 0:
+        if i % 100 == 0:  # check accuracy every 100 steps
             print('ON STEP: {}'.format(i))
             print('ACCURACY: ')
             matches = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y_true, 1))
-            acc = tf.reduce_mean(tf.cast(matches, tf.float32))
+            acc = tf.reduce_mean(tf.cast(matches, tf.float32))  # booleans to float
 
-            print(sess.run(acc, feed_dict={x: mnist.test.images, y_true: mnist.test.labels, hold_prob: 1.0}))
+            print(sess.run(acc, feed_dict={x: mnist.test.images, y_true: mnist.test.labels, hold_prob: 1.0}))  # no drop
             print('\n')
